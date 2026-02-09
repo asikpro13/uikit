dce760ab5ea7632b4cc39110ade6219ca9077270
Vadim Mazurek <asikpro13@mail.ru>
2026-02-09 22:09:52 +0500

    uikit

--- lib/uikit.dart
+++ lib/uikit.dart
@@ -5,3 +5,12 @@ library uikit;
 export 'colors.dart';
 export 'styles.dart';
 export 'widgets/custom_icon.dart';
+export 'widgets/custom_bubble.dart';
+export 'widgets/custom_button.dart';
+export 'widgets/custom_card.dart';
+export 'widgets/custom_counter.dart';
+export 'widgets/custom_header.dart';
+export 'widgets/custom_modal.dart';
+export 'widgets/custom_search.dart';
+export 'widgets/custom_snackbar.dart';
+
