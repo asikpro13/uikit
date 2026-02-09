c2a7bb2c0739d98f7173115507040c6483f20e07
Vadim Mazurek <asikpro13@mail.ru>
2026-02-09 19:18:06 +0500

    uikit

--- lib/widgets/custom_bubble.dart
+++ lib/widgets/custom_bubble.dart
@@ -10,13 +10,14 @@ class CustomBubble extends StatelessWidget {
   final Color backgroundBubble; //
   final double radiusBubble; //
 
-  const CustomBubble({super.key, required this.backgroundBubble, required this.radiusBubble, required this.pathBubble, required this.onBubble, required this.widthBubble, required this.heightBubble});
+  const CustomBubble({super.key, required this.backgroundBubble, required this.radiusBubble, required this.pathBubble, required this.onBubble, required this.widthBubble, required this.heightBubble,});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onBubble,
       child: Container(
+        width: widthBubble + 5,
         decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(radiusBubble)), color: backgroundBubble),
         child: SvgPicture.asset(pathBubble, width: widthBubble, height: heightBubble),
       ),
